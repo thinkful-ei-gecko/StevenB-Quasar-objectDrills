5beed7118519b625d649e39ee026e0c576478ecc
Quasar Wei <quasar.wei@outlook.com>
2019-08-14 12:42:42 -0700

    problem 7

--- objectDrills.js
+++ objectDrills.js
@@ -69,3 +69,58 @@ function peopleWithBoss(bossIncludedArray) {
 }
 
 //peopleWithBoss(arrayOfPeopleWithBoss);
+
+// 6. Cracking the Code
+
+// use an object as your cipher. Instead of doing some kind 
+// of condition check like if (char === 'a'), 
+// you should use an object's key-value pair structure as the code translator. 
+
+// this decodes a word into a single char based on the first letter
+const decoderKey = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4
+};
+
+function decode(str) {
+  // take the first letter in str  ex: 'b'
+  // check if the letter is a key in decoderKey
+  // if it is a key, then we return the letter at index 'value'
+
+  for(let key in decoderKey) {
+    if (str[0] === key) {
+      return str[decoderKey[key]];
+    }
+  } 
+  return ' ';
+}
+
+function decodeWords(sentence) {
+  let words = sentence.split(' ');
+
+  return words.reduce( (accumulator, currentValue) => { 
+    return accumulator + decode(currentValue);
+  }, '');
+}
+
+let string = 'craft block argon meter bells brown croon droop';
+console.log(decodeWords(string));
+
+
+// 7. write a character builder factory function 
+
+function createCharacter() {
+
+  // describe method
+    // no parameters
+    // prints out string "{name} is a {race} from {origin}."
+
+  // evaluateFight method
+    // takes charactrer object
+    // returns string: "Your opponent takes {x} damage and you receive {y} damage"
+
+
+  
+  // create array of characters that calls createCharacters()}
